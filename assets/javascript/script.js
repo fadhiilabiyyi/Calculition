e8cac0e97e7c2877c5f7e47d7c124d56fafd9b2f
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-04-04 11:24:16 +0700

    Change BMI Page from PHP to HTML

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+const message = getElementById("message").value;
+
 // clearScreen => Clear all the values
 function clearScreen() {
     document.getElementById("result") . value = "";
@@ -14,4 +16,23 @@ function calculate() {
     const q = eval(p);
 
     document.getElementById("result") . value = q;
+}
+
+function calcBmi() {
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value / 100;
+    const message = document.getElementById("message").innerHTML;
+
+    const result = document.getElementById("result").value = weight / (height * height);
+    
+    if (result > 25.1) {
+        document.getElementById("message").innerHTML = "Gemuk";
+    } else if (result < 25.1 && result > 18.4) {
+        document.getElementById("message").innerHTML = "Normal";
+    } else if (result <= 18.4) {
+        document.getElementById("message").innerHTML = "Kurus";
+    } else {
+        message = "Silahkan masukkan data dengan benar!";
+    }
+    
 }
